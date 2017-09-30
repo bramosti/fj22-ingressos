916cd154c15ad2889d218e14b83e471f6b6acf48
Bruno Ramos <brunorr@gmail.com>
2017-09-29 21:23:02 -0300

    adicionando classes

---
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoEstudante.java
@@ -0,0 +1,15 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import java.math.BigDecimal;
+
+public class DescontoEstudante implements Descontos {
+	
+	private BigDecimal metade = new BigDecimal("2.0");
+
+	@Override
+	public BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal) {
+		// TODO Auto-generated method stub
+		return  precoOriginal.divide(metade);
+	}
+
+}
